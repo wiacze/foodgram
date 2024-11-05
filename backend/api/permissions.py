47bf6491156d535925049d6b94ffac03a3108dd3
wiacze <151467867+wiacze@users.noreply.github.com>
2024-11-05 14:07:55 +0300

    Add error message for permission

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminIsAuthorOrReadOnly(BasePermission):
 
+    message = 'У вас недостаточно прав для данного действия.'
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in SAFE_METHODS or request.user.is_authenticated

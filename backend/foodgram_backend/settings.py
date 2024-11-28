5750d340e1be25620f317ce877284feee78460b8
wiacze <151467867+wiacze@users.noreply.github.com>
2024-11-28 14:13:35 +0300

    Update settings.py

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -83,6 +83,16 @@ DATABASES = {
     }
 }
 
+# If need sqlite 
+ 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         # 'NAME': '/data/db.sqlite3',
+#     }
+# }
+
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
